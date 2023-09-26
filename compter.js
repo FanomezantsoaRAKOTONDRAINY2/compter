e8d6a149723850d91b4302f039e20a0e61512d12
Fanomezantsoa <fanelsodzy@gmail.com>
2023-09-26 22:22:28 +0200

    je fais tout javascripte

--- compter.js
+++ compter.js
@@ -1,6 +1,29 @@
-const spn = document.getElementById('spn'),
-      btn1 = document.getElementById('btn1');
+//initialisation
+var compter = 0;
+//selection les élément html
+var spn = document.getElementById('spn');
+var parent_btn = document.querySelectorAll('.btn');
 
-const compt = [haloko,tiko];
-btn1.addEventListener('click', function(){
+parent_btn.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        const styles = e.currentTarget.classList;
+        if (styles.contains('btn1')) {           
+            compter--;
+        }
+        else if (styles.contains('btn3')) {
+            compter ++;
+        } else {
+            compter = 0;
+        }
+        spn.textContent = compter;
+        if (compter >0) {
+            spn.style.color = 'green'
+        }
+        else if (compter<0) {
+            spn.style.color = 'red';
+        }else{
+            spn.style.color = 'rgb(136, 175, 162)'
+        }
+
+    })
 })
